f43b9bd8ec739c2123d3ece7dc017f3d30d6421f
ashleyteng <ateng@princeton.edu>
2022-02-22 20:18:17 -0500

    edit test.py

--- test.py
+++ test.py
@@ -35,7 +35,7 @@ output_idx = outputs[1:].squeeze(1).argmax(1)
 # itos: A list of token strings indexed by their numerical identifiers.
 predicted_translation = ' '.join([trg_en.vocab.itos[idx] for idx in output_idx])
 print('TRANSLATION: ', predicted_translation)
-print('BLEU SCORE: ', bleu_score(predicted_translation, target_translation))
+# print('BLEU SCORE: ', bleu_score(predicted_translation, target_translation))
 # test_loss = evaluate(model, test_iterator, criterion)
 #
 # print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
