b6545824a92cf724d6acb6c459387b8f38290fb9
Yun Teng <mindtracker.yun24@gmail.com>
2022-02-22 17:41:57 -0800

    edit test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from training_functions import evaluate
 from build_model import build_model
 from setup import get_data, get_iterators
 from torchtext.data.metrics import bleu_score
+from random import randrange
 
 input_dim = 28
 output_dim = 37
@@ -15,7 +16,7 @@ _, _, test_iterator, src_tw, trg_en = get_iterators(train_data, valid_data, test
 model = build_model(len(src_tw.vocab), len(trg_en.vocab))
 model.load_state_dict(torch.load('translation-model.pt'))
 
-example_idx = 0
+example_idx = randrange(len(train_data.examples))
 example = train_data.examples[example_idx]
 print('SOURCE: ', ' '.join(example.src))
 target_translation = ' '.join(example.trg)
