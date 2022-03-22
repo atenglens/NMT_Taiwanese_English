fa12584d02c93ef17fe565e1ec99e4d2a68f5c79
ashleyteng <ateng@princeton.edu>
2022-03-21 20:13:08 -0400

    experiment

--- seq2seq/tokenizer.py
+++ seq2seq/tokenizer.py
@@ -26,9 +26,9 @@ def tokenize_tw(text):
     for specialChar in specialChars:
         text = text.replace(specialChar, '')
     text = text.replace('-', ' ')
-    isolate_periods = re.split(r'([.])', text)
-    strip = [x.lower().strip() for x in isolate_periods]
-    return strip[::-1]
+    # isolate_periods = re.split(r'([.,])', no_hyphens)
+    # strip = [x.strip() for x in isolate_periods]
+    return text.lower().split() # strip[::-1]
 
 # def tokenize_tw(text):
 #     """
